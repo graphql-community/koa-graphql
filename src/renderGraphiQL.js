927157b73c0799bb23456d49ee44f9d684fec7d0
C. T. Lin <chentsulin@gmail.com>
2016-04-10 15:09:25 +0800

    Use ReactDOM.render

--- src/renderGraphiQL.js
+++ src/renderGraphiQL.js
@@ -129,7 +129,7 @@ add "&raw" to the end of the URL within a browser.
     }
 
     // Render <GraphiQL /> into the body.
-    React.render(
+    ReactDOM.render(
       React.createElement(GraphiQL, {
         fetcher: graphQLFetcher,
         onEditQuery: onEditQuery,

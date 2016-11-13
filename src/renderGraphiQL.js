abed9ee965266daa6654e98456b5ffa3415aa4a5
C.T. Lin <chentsulin@gmail.com>
2016-11-13 11:37:58 +0800

    Use the latest version of graphiql

--- src/renderGraphiQL.js
+++ src/renderGraphiQL.js
@@ -8,7 +8,7 @@ type GraphiQLData = {
 };
 
 // Current latest version of GraphiQL.
-const GRAPHIQL_VERSION = '0.7.3';
+const GRAPHIQL_VERSION = '0.8.0';
 
 // Ensures string values are save to be used within a <script> tag.
 function safeSerialize(data): string {

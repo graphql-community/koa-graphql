5f4ac522cb7ff63dc9521bb0142148d22090d90c
chentsulin <chentsulin@gmail.com>
2018-10-30 01:46:08 +0800

    mocha >= 4.0 correctly reports unhandledRejection on its own

--- resources/mocha-bootload.js
+++ resources/mocha-bootload.js
@@ -3,8 +3,3 @@
 require('babel-register')({
   plugins: ['transform-async-to-generator', 'transform-runtime'],
 });
-
-process.on('unhandledRejection', function(error) {
-  console.error('Unhandled Promise Rejection:');
-  console.error((error && error.stack) || error);
-});

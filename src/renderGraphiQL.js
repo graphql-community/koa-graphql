cbf656e3b2149a0849fe49d101eb7c5329ed55b2
Yanick Rochon <yanick.rochon@gmail.com>
2020-04-27 13:59:32 -0400

    Updating GraphiQL to latest

--- src/renderGraphiQL.js
+++ src/renderGraphiQL.js
@@ -8,7 +8,7 @@ type GraphiQLData = {
 };
 
 // Current latest version of GraphiQL.
-const GRAPHIQL_VERSION = '0.12.0';
+const GRAPHIQL_VERSION = '0.17.5';
 
 // Ensures string values are safe to be used within a <script> tag.
 function safeSerialize(data): string {

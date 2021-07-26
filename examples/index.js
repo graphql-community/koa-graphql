af0885ba138af81897f93c3228bda0c64b294418
C. T. Lin <chentsulin@gmail.com>
2021-07-27 00:28:52 +0800

    ci: migrate from travis CI to GitHub Actions (#145)
    * ci: migrate from travis CI to GitHub Actions
    
    * ci: upgrade codecov-action to v2

--- examples/index.js
+++ examples/index.js
@@ -1,22 +1,34 @@
+/* eslint-disable no-console */
 import Koa from 'koa';
 import mount from 'koa-mount';
 import graphqlHTTP from '../src/';
 import { buildSchema } from 'graphql';
- // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
- // The root provides a resolver function for each API endpoint
-var root = {
+
+// The root provides a resolver function for each API endpoint
+const root = {
   hello: () => 'Hello world!',
 };
-var app = new Koa();
-app.use(mount('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-})));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+const app = new Koa();
+
+app.use(
+  mount(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: true,
+    }),
+  ),
+);
+
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+});

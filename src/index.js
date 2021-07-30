12b50a32dd9befee0ff70e1ddadabbbc00fe226e
chentsulin <chentsulin@gmail.com>
2021-07-30 13:18:10 +0800

    Remove unnecessary string concatenations

--- src/index.js
+++ src/index.js
@@ -191,8 +191,7 @@ function graphqlHTTP(options: Options): Middleware {
       // Assert that optionsData is in fact an Object.
       if (!optionsData || typeof optionsData !== 'object') {
         throw new Error(
-          'GraphQL middleware option function must return an options object ' +
-            'or a promise which will be resolved to an options object.',
+          'GraphQL middleware option function must return an options object or a promise which will be resolved to an options object.',
         );
       }
 
@@ -303,8 +302,7 @@ function graphqlHTTP(options: Options): Middleware {
             response.set('Allow', 'POST');
             throw httpError(
               405,
-              `Can only perform a ${operationAST.operation} operation ` +
-                'from a POST request.',
+              `Can only perform a ${operationAST.operation} operation from a POST request.`,
             );
           }
         }

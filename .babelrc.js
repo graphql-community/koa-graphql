2c0871ff570ffcbb9d5f0fa7261e3a19a9eb9137
chentsulin <chentsulin@gmail.com>
2021-08-02 00:18:40 +0800

    babel: Switch to using babel-env

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,7 @@
 module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
   plugins: [
     './resources/load-staticly-from-npm.js',
     '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-transform-modules-commonjs',
   ],
 };

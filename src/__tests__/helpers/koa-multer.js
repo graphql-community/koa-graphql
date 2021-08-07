bc23c7c6fb0bffa02fdfbaefb0f2f0f993ab98c1
chentsulin <chentsulin@gmail.com>
2021-08-07 17:36:27 +0800

    use util.promisify instead of thenify

--- src/__tests__/helpers/koa-multer.js
+++ src/__tests__/helpers/koa-multer.js
@@ -1,6 +1,7 @@
 // @flow strict
 
-import thenify from 'thenify';
+import util from 'util';
+
 import multer from 'multer';
 
 export default function multerWrapper(options) {
@@ -8,8 +9,8 @@ export default function multerWrapper(options) {
   const _single = upload.single.bind(upload);
   upload.single = function (param) {
     return async function (ctx, next) {
-      const thenified = thenify(_single(param));
-      await thenified(ctx.req, ctx.res);
+      const promisified = util.promisify(_single(param));
+      await promisified(ctx.req, ctx.res);
       return next();
     };
   };

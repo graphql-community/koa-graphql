d61d1ffda876a92da99be75f008b005e7c7722b7
chentsulin <chentsulin@gmail.com>
2021-11-24 16:50:44 +0800

    Fix NPM package on Node12

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  presets: [['@babel/preset-env', { targets: { node: '12' } }]],
   plugins: [
     './resources/load-staticly-from-npm.js',
     '@babel/plugin-transform-flow-strip-types',
+  ],
+  overrides: [
+    {
+      include: ['**/__tests__/**/*'],
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+    },
   ],
 };
